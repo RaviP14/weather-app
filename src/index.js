501f033b8edfe0a8e3b048e1f58fc4118939e149
RaviP14 <ravi.patel14@outlook.com>
2021-10-04 18:43:21 +0100

    User can search for locations and receive data via the console.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import { getWeather } from './weather';
 import getData from './data';
-
+import elements from './elements';
 /* should work fine in weather.js babel had an issue, need location & units for Celcius & fahrenheit
 can also get longetude and lat form this json file so we can use in other api for hourly forcast */
 // try weather.js function then delete below function.
-
-getWeather.temp1('london', 'metric').then((data) => {
-  console.log(data);
-  getData.dataProcess(data, 'main').then((value) => {
-    console.log(value);
+(() => {
+  elements.locate.addEventListener('click', (e) => {
+    e.preventDefault();
+    const find = elements.location.value;
+    const string = find.toLowerCase();
+    getWeather.temp1(string, 'metric').then((data) => {
+      console.log(data);
+      getData.dataProcess(data, 'main').then((value) => {
+        console.log(value);
+      });
+      getData.dataProcess(data, 'coord').then((value) => {
+        console.log(value);
+      });
+      getData.dataProcess(data, 'sys').then((value) => {
+        console.log(value);
+      });
+      getData.dataProcess(data, 'name').then((value) => {
+        console.log(value);
+      });
+    });
   });
-  getData.dataProcess(data, 'coord').then((value) => {
-    console.log(value);
-  });
-  getData.dataProcess(data, 'sys').then((value) => {
-    console.log(value);
-  });
-  getData.dataProcess(data, 'name').then((value) => {
-    console.log(value);
-  });
-});
+})();
