a96b8c35ab74186122a202398584b24f759cd37f
RaviP14 <ravi.patel14@outlook.com>
2021-10-05 14:34:51 +0100

    Added 2nd api for daily & hourly weather forecasts.

--- src/weather.js
+++ src/weather.js
@@ -20,8 +20,31 @@ const getWeather = (() => {
       return err;
     }
   }
+
+  async function getForecast(lat, lon, type, exclude) {
+    try {
+      const address = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${type}&exclude=${exclude}&appid=7474fb5568726a003e49583bf2111de8`;
+      const respone = await fetch(address, { mode: 'cors' });
+      const forecastData = await respone.json();
+      return forecastData;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  async function temp2(lat, lon, type, exclude) {
+    try {
+      const data = await getForecast(lat, lon, type, exclude);
+      return data;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
   return {
     temp1,
+    temp2,
   };
 })();
 
