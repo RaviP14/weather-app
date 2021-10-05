aac8f5d3511bc560a5e3c717819b5725c7c051b1
RaviP14 <ravi.patel14@outlook.com>
2021-10-05 17:21:41 +0100

    Added form reset & added temp data to divs as text.

--- src/elements.js
+++ src/elements.js
@@ -1,13 +1,38 @@
 const elements = (() => {
+  const searchForm = document.getElementById('searchForm');
+  searchForm.setAttribute('autocomplete', 'off');
+  const locationForm = searchForm;
+
   const searchBtn = document.getElementById('searchbtn');
   const locate = searchBtn;
 
   const searchInput = document.getElementById('searchInput');
   const location = searchInput;
 
+  const textLocation = document.getElementById('locationText');
+  const cityText = textLocation;
+
+  const todayTemp = document.getElementById('todayTemp');
+  const cityTemp = todayTemp;
+
+  const feelsLike = document.getElementById('feelsLike');
+  const feelsLikeTemp = feelsLike;
+
+  const maxTemp = document.getElementById('maxTemp');
+  const cityMaxTemp = maxTemp;
+
+  const minTemp = document.getElementById('minTemp');
+  const cityMinTemp = minTemp;
+
   return {
     locate,
     location,
+    locationForm,
+    cityText,
+    cityTemp,
+    feelsLikeTemp,
+    cityMaxTemp,
+    cityMinTemp,
   };
 })();
 
--- src/index.js
+++ src/index.js
@@ -9,10 +9,15 @@ can also get longetude and lat form this json file so we can use in other api fo
     e.preventDefault();
     const find = elements.location.value;
     const string = find.toLowerCase();
+
     getWeather.temp1(string, 'metric').then((data) => {
       console.log(data);
       getData.dataProcess(data, 'main').then((value) => {
         console.log(value);
+        elements.cityTemp.textContent = value.temp;
+        elements.feelsLikeTemp.textContent = `Feels like: ${value.feels_like}`;
+        elements.cityMaxTemp.textContent = `Max: ${value.temp_max}`;
+        elements.cityMinTemp.textContent = `Min: ${value.temp_min}`;
       });
       getData.dataProcess(data, 'coord').then((value) => {
         console.log(value);
@@ -29,7 +34,10 @@ can also get longetude and lat form this json file so we can use in other api fo
       });
       getData.dataProcess(data, 'name').then((value) => {
         console.log(value);
+        elements.cityText.textContent = value;
       });
     });
+
+    elements.locationForm.reset();
   });
 })();
