2ddc610689eb6fa75a7342612da50ec22807133b
RaviP14 <ravi.patel14@outlook.com>
2021-10-07 20:07:19 +0100

    Added two functions, 1 to get the current hour and 2nd to filter an array using hours.

--- src/data.js
+++ src/data.js
@@ -9,8 +9,31 @@ const getData = (() => {
       return err;
     }
   }
+  // get current hour.
+  function getHours() {
+    const date = new Date();
+    const now = date.getHours();
+    return now;
+  }
+
+  function hourNow() {
+    const time = getHours();
+    return time;
+  }
+  // filter hours array to give temp from current hour.
+  function filterArray(array, hour) {
+    const filteredArr = array.filter((_, index) => index >= hour);
+    return filteredArr;
+  }
+
+  async function weatherFrom(array, hour) {
+    const finalArr = await filterArray(array, hour);
+    return finalArr;
+  }
   return {
     dataProcess,
+    hourNow,
+    weatherFrom,
   };
 })();
 
