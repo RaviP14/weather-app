c888f597df460c3b847286dc7b22d71454f71b00
RaviP14 <ravi.patel14@outlook.com>
2021-10-11 18:38:59 +0100

    Now doesn't appear twice on the hourly forecast table.

--- src/table.js
+++ src/table.js
@@ -1,18 +1,20 @@
 import getData from './data';
 
 const buildTable = (() => {
-  // make async function
   function newTable(table, array, key) {
     const row = table.insertRow(0);
     const row2 = table.insertRow(1);
     const currentTime = getData.hourNow();
     let time = getData.hourNow();
+    let now = true;
+    // Filter array using current time.
     const weatherArr = getData.weatherFrom(array, time);
     weatherArr.then((value) => {
       value.forEach((element) => {
         const val1 = row.insertCell();
-        if (time === currentTime) {
+        if (time === currentTime && now === true) {
           val1.textContent = 'Now';
+          now = false;
         } else if (time <= 24) {
           val1.textContent = time;
         } else {
