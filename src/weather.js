aba30b3cd09518dccd6f09d4586e3d2cc30bb52a
RaviP14 <ravi.patel14@outlook.com>
2021-10-23 19:51:07 +0100

    Resolved issue with mixed loading error for gh pages.

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,7 @@
 const getWeather = (() => {
   async function getTemp(location, type) {
     try {
-      const address = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${type}&appid=7474fb5568726a003e49583bf2111de8`;
+      const address = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${type}&appid=7474fb5568726a003e49583bf2111de8`;
       const repsonse = await fetch(address, { mode: 'cors' });
       const tempData = await repsonse.json();
       return tempData;
